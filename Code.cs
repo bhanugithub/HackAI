73cd49142610b3cfeb9c305d12f40e2fc9c81d49
bhanugithub <bhanus506@gmail.com>
2024-03-13 21:29:22 +0530

    Update Code.cs

--- Code.cs
+++ Code.cs
@@ -8,16 +8,11 @@ class Program
         int num2 = 10;
         int total = Add(num1, num2);
         Console.WriteLine("The total is: " + total);
-        ProvideFeedback(total);
     }
 
     static int Add(int x, int y)
     {
-        return x + y;
-    }
-
-    static void ProvideFeedback(int result)
-    {
+        int result = x + y;
         Console.WriteLine("The result is: " + result);
         if (result > 10)
             Console.WriteLine("Wow! That's a big number!");
@@ -25,5 +20,6 @@ class Program
             Console.WriteLine("That's a decent number.");
         else
             Console.WriteLine("Eh, that's a small number.");
+        return result;
     }
 }
